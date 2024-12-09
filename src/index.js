4ccae661903d32c7603ae4a1b8ebf336bdc84351
unknown <ram.gk@metapercept.com>
2024-12-09 14:28:52 +0530

    added updated code on 09 dec

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 // Import Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -17,4 +17,4 @@ root.render(
 );
 
 
-reportWebVitals();
+
